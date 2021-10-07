e4386ad339c85d09fea86ce9d721128982d20141
tryingtokeepup <kl.hawaii@gmail.com>
2021-10-06 18:26:03 -0700

    pushing react client

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
-// use the packages above
+const path = require('path');
+// use the packages above - global middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 const port = process.env.PORT || 9000;
 
 app.use('/api/*', (_, res) => {
   res.json({ data: "This is Sayo's data lol" });
+});
+
+app.use('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => {
